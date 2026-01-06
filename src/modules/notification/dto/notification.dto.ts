0bd3708d4dd9c45adf8bec6e1c46efdf74824388
supakorn542 <supakorn542@gmail.com>
2026-01-06 13:34:33 +0700

    add type in NotificationResponse

--- src/modules/notification/dto/notification.dto.ts
+++ src/modules/notification/dto/notification.dto.ts
@@ -16,7 +16,7 @@ export class NotificationResponseDto {
   @ApiProperty({ example: false })
   isRead: boolean;
 
-  @ApiProperty({ example: 'uuid-task-id-5678', nullable: true })
+  @ApiProperty({ example: 'uuid-task-id-5678', nullable: true, type: 'string' })
   taskId: string | null;
 
   @ApiProperty({ example: '2025-12-30T09:00:00.000Z' })
